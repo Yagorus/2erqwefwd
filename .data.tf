d994a46c27bbaa9b152b5eaa058fcf3ca26ff274
Yagorus <yegor.sidenko@gmail.com>
2021-12-15 03:14:17 +0200

    sd

--- .data.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_profile_access_key" {
-  description = "aws access key"
-  default = "AKIA26XYZMMBHL25FWER"
-}
-
-variable "aws_profile_secret_key" {
-  description = "aws access key"
-  default = "y//KMbdyHnJnLaHALJJ8H+5/K3RivzdBiAL0VQz4"
-}
