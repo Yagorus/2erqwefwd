f7f553bd4960718e6dd7133377a14e81f3ed5b1f
Yagorus <yegor.sidenko@gmail.com>
2021-12-15 04:13:27 +0200

    asdASDFAS

--- network.tf
+++ network.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+    region      = var.aws_region
+    access_key  = var.aws_profile_access_key
+    secret_key  = var.aws_profile_secret_key
+}
+
 data "aws_availability_zones" "available" {
 
 }
