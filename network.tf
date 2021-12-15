481e8ab0555d73e7123fc36397b62ec356c941ae
Yagorus <yegor.sidenko@gmail.com>
2021-12-15 05:40:26 +0200

    sdfsdf

--- network.tf
+++ network.tf
@@ -47,33 +47,32 @@ resource "aws_subnet" "private" {
 }
 
 resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-  tags = {
+    vpc_id = aws_vpc.main.id
+    tags = {
     Name = "gw"
   }
 }
 
 resource "aws_route" "internet_access" {
     #what vpc use for route
-  route_table_id         = aws_vpc.main.main_route_table_id
+    route_table_id         = aws_vpc.main.main_route_table_id
     #add routes 
-  destination_cidr_block = "0.0.0.0/0"
+    destination_cidr_block = "0.0.0.0/0"
     #define gateway to internet
-  gateway_id             = aws_internet_gateway.gw.id
+    gateway_id             = aws_internet_gateway.gw.id
 }
-/*
+
 resource "aws_eip" "gw" {
-  count = 1
-  vpc = true
-  depends_on = [aws_internet_gateway.gw] 
-  tags = {
+    count      =  var.az_count
+    vpc = true
+    depends_on = [aws_internet_gateway.gw] 
+    tags = {
     Name = "EIP"
   }
-
 }
 
 resource "aws_nat_gateway" "gateway" {
-  count         = 1
+  count         =  var.az_count
   subnet_id     = element(aws_subnet.public.*.id, count.index)
   allocation_id = element(aws_eip.gw.*.id, count.index)
   tags = {
@@ -81,6 +80,7 @@ resource "aws_nat_gateway" "gateway" {
   }
 }
 
+/*
 resource "aws_route_table" "private" {
   count = "1" 
   vpc_id = aws_vpc.main.id
