6d83373c9509e5efdd85300422c9a1dd4ce823d5
Yagorus <yegor.sidenko@gmail.com>
2021-12-18 17:47:46 +0200

    asdfsadf

--- network.tf
+++ network.tf
@@ -82,7 +82,7 @@ resource "aws_route_table" "private" {
 
 resource "aws_route_table_association" "private" {
   count = var.az_count
-  subnet_id     = element(aws_subnet.public.*.id, count.index)
+  subnet_id     = element(aws_subnet.private.*.id, count.index)
   route_table_id =element(aws_route_table.private.*.id, count.index)
   #route_table_id = aws_route_table.private[count.index].id
 }
