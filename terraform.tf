d7e4b1de97d2bc5f5391bfdbb3ce3c7b008d9862
Yagorus <yegor.sidenko@gmail.com>
2021-12-18 16:07:39 +0200

    dsafasd

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,8 @@
 
 provider "aws" {
     region      = var.aws_region
-    profile     = "/home/yegor/.aws/credentials"
-    #shared_credentials_file = "/home/yegor/.aws/credentials"
+    profile     = "terraform-user"
+    shared_credentials_file = "/home/yegor/.aws/credentials"
 
     #access_key  = var.aws_profile_access_key
     #secret_key  = var.aws_profile_secret_key
