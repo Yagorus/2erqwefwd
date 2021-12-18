554366380fd5ed56af31a33299eece2d4dee03dc
Yagorus <yegor.sidenko@gmail.com>
2021-12-18 17:46:21 +0200

    adsfsdf

--- network.tf
+++ network.tf
@@ -79,11 +79,10 @@ resource "aws_route_table" "private" {
   }
 }
 
-/*
+
 resource "aws_route_table_association" "private" {
   count = var.az_count
   subnet_id     = element(aws_subnet.public.*.id, count.index)
   route_table_id =element(aws_route_table.private.*.id, count.index)
   #route_table_id = aws_route_table.private[count.index].id
 }
-*/
