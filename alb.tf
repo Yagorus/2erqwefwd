8287884f6d5e4f39e88b20b8bb2fd886b37700eb
Yagorus <yegor.sidenko@gmail.com>
2021-12-27 08:46:51 +0200

    adsf

--- alb.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_alb" "main" {
-  name            = "${var.app_name}-${var.environment}-lb"
-  subnets         = aws_subnet.public.*.id
-  security_groups = [aws_security_group.lb.id]
-}
