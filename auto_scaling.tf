55902d235e1246d9d85d85d2894f547de374a937
Yagorus <yegor.sidenko@gmail.com>
2021-12-27 07:21:52 +0200

    asdf

--- auto_scaling.tf
+++ auto_scaling.tf
@@ -1,19 +1,14 @@
 resource "aws_launch_configuration" "launch" {
     name = "launch"
-    image_id = data.aws_ami.ubuntu.id
+    image_id = data.aws_ami_ids.ubuntu.id
     instance_type = "t2.micro"
 }
 
 resource "aws_autoscaling_group" "autoscaling" { 
   depends_on                = [aws_launch_configuration.launch]
-  count                     = var.az_count
   name                      = "autoscaling"
   max_size                  = 1
   min_size                  = 1
-  health_check_grace_period = 300
-  health_check_type         = "ELB"
-  desired_capacity          = 1
-  force_delete              = true
   launch_configuration      = aws_launch_configuration.launch.name
   vpc_zone_identifier       = [aws_subnet.public[0].id, aws_subnet.public[1].id] 
 
@@ -22,5 +17,4 @@ resource "aws_autoscaling_group" "autoscaling" {
     value               = "${var.app_name}-VPC-autoscaling-group"
     propagate_at_launch = true
   }
-  
 }
--- data.tf
+++ data.tf
@@ -5,18 +5,22 @@ data "aws_availability_zones" "available" {
 data "aws_ami" "ubuntu" {
     most_recent = true
     owners = ["753214776066"]
+
     filter {
         name   = "name"
         values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
     }
-
     filter {
         name = "virtualization-type"
         values = ["hvm"]
     }
+}
+data "aws_ami_ids" "ubuntu" {
+  owners = ["753214776066"]
 
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/ubuntu-*-*-amd64-server-*"]
+  }
 }
 
-output "ubuntu_ami" {
-  value = data.aws_ami.ubuntu
-}
