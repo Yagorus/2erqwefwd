3c1ce673607652839a75ece68c482aa0c175b3b8
Yagorus <yegor.sidenko@gmail.com>
2021-12-27 08:45:44 +0200

    adsf

--- auto_scaling.tf
+++ auto_scaling.tf
@@ -1,14 +1,14 @@
 resource "aws_launch_configuration" "launch" {
     name = "launch"
     image_id =  "ami-0d527b8c289b4af7f"
-    security_groups = aws_security_group.lb
+    security_groups = [aws_security_group.asg.id]
     instance_type = "t2.micro"
 }
 
 resource "aws_autoscaling_group" "autoscaling" { 
   depends_on                = [aws_launch_configuration.launch]
   count                     = var.az_count
-  name                      = "autoscaling"
+  name                      = "bastion-server-as"
   max_size                  = 1
   min_size                  = 1
   launch_configuration      = aws_launch_configuration.launch.name
--- security.tf
+++ security.tf
@@ -1,12 +1,12 @@
-resource "aws_security_group" "lb" {
-  name        = "${var.app_name}-${var.environment}-lb-sg"
+resource "aws_security_group" "asg" {
+  name        = "${var.app_name}-${var.environment}-asg-sg"
   description = "controls access to the ALB"
   vpc_id      = aws_vpc.main.id
 
   ingress {
     protocol    = "tcp"
-    from_port   = var.app_port
-    to_port     = var.app_port
+    from_port   = 22
+    to_port     = 22
     cidr_blocks = ["0.0.0.0/0"]
   }
 
