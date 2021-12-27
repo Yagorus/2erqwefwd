de466bd9763bd62fe10eb743de73a73d13b42544
Yagorus <yegor.sidenko@gmail.com>
2021-12-27 04:36:59 +0200

    asdfasdf

--- data.tf
+++ data.tf
@@ -1,12 +1,12 @@
 data "aws_availability_zones" "available" {
 
 }
-data "aws_ami" "ubuntu20" {
+data "aws_ami" "ubuntu" {
     most_recent = true
     owners = ["753214776066"]
     filter {
-        name = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-20.04-amd64-server-*"]
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-20.04-amd64-server-*"]
     }
 
 }
