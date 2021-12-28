857ca89076dcfd159c47bd0130280511737aafa3
Yagorus <yegor.sidenko@gmail.com>
2021-12-28 23:26:23 +0200

    asdf

--- ec2.tf
+++ ec2.tf
@@ -10,16 +10,19 @@ resource "aws_instance" "name" {
   }
 }
 */
-    resource "aws_instance" "name" {
-        depends_on  = [aws_security_group.asg, aws_subnet.public]
-        count         = var.az_count
-        subnet_id =  element(aws_subnet.public.*.id, count.index)
+resource "aws_instance" "name" {
+    depends_on  = [aws_security_group.asg, aws_subnet.public]
+    count         = var.az_count
+    subnet_id =  element(aws_subnet.public.*.id, count.index)
 
-        image_id = "ami-0d527b8c289b4af7f"
-        instance_type = "t2.micro"
+    ami = "ami-0d527b8c289b4af7f"
+    instance_type = "t2.micro"
 
-        security_groups = [aws_security_group.asg.id]
-        
-        user_data = file("user_data.sh")
-        name = "launch"
+    security_groups = [aws_security_group.asg.id]
+    
+    user_data = file("user_data.sh")
+
+    tags = {
+        Name = "${var.app_name}-ec2"
+    }
 }
