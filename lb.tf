36f914e4fc7b2fe8aa1df416f29d252649f6053e
Yagorus <yegor.sidenko@gmail.com>
2021-12-28 18:42:03 +0200

    asdf

--- lb.tf
+++ lb.tf
@@ -1,30 +1,10 @@
-resource "aws_elb" "main" {
-  #depends_on      = [aws_autoscaling_group.autoscaling]
+
+resource "aws_alb" "main" {
   name            = "${var.app_name}-${var.environment}-lb"
-  #subnets         = aws_subnet.public.*.id
-  security_groups = [aws_security_group.asg.id]
-  availability_zones = [element(data.aws_availability_zones.available.names[*], var.az_count)]
-  listener {
-    instance_port     = 80
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
-  }
-
-
-  health_check {
-    healthy_threshold   = "2"
-    unhealthy_threshold = "2"
-    timeout             = "3"
-    target = "HTTP:80/"
-    interval            = "10"
-  }
-  tags = {
-    Name = "${var.app_name}-${var.environment}-lb"
-  }
-
+  subnets         = aws_subnet.public.*.id
+  security_groups = [aws_security_group.lb.id]
 }
-/*
+
 resource "aws_alb_target_group" "app" {
   name        = "${var.app_name}-${var.environment}-tg"
   port        = 80
@@ -53,4 +33,3 @@ resource "aws_alb_listener" "front_end" {
     type             = "forward"
   }
 }
-*/
