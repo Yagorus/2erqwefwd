7d0fc352b3849b1354cba9769fd292a74da744a0
Yagorus <yegor.sidenko@gmail.com>
2021-12-28 23:16:23 +0200

    asdf

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "name" {
   ami           = "ami-0d527b8c289b4af7f"
   instance_type = "t2.micro"
 
-  subnet_id =  [element(aws_subnet.public.*.id, count.index)]
+  subnet_id =  element(aws_subnet.public.*.id, count.index)
   tags = {
     Name = "${var.app_name}-ec2"
   }
