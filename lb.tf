1d2fd79b60d9dcfcfbd226f720c19e3b6feaba4f
Yagorus <yegor.sidenko@gmail.com>
2021-12-29 08:05:43 +0200

    asdf

--- lb.tf
+++ lb.tf
@@ -27,7 +27,7 @@ resource "aws_alb_target_group" "app" {
 }
 
 resource "aws_alb_listener" "front_end" {
-  load_balancer_arn = aws_alb.main.arn
+  load_balancer_arn = aws_lb.main.arn
   port              = 80
   protocol          = "HTTP"
 
