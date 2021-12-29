3658c6d4910d7962c2992a767cc78f721854acaa
Yagorus <yegor.sidenko@gmail.com>
2021-12-29 07:31:53 +0200

    sadf

--- security.tf
+++ security.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "ec2" {
+resource "aws_security_group" "asg" {
   name        = "${var.app_name}-${var.environment}-asg-sg"
   description = "controls access to the ALB"
   vpc_id      = aws_vpc.main.id
@@ -44,49 +44,3 @@ resource "aws_security_group" "lb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-/*
-resource "aws_security_group" "lb" {
-  name        = "${var.app_name}-${var.environment}-lb-sg"
-  description = "controls access to the ALB"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    protocol    = "tcp"
-    from_port   = var.app_port
-    to_port     = var.app_port
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "lb" {
-    name = "Dybnamic SG"
-
-    dynamic "ingress" {
-      for_each = ["80","8000"]
-      content {
-        from_port = ingress.value
-        to_port   = ingress.value
-        protocol  = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-      }
-    }
-    egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "sg-lb"
-  }
-}
-*/
