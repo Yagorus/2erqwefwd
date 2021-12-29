1ec7aba6a1f9a46f480b7215e73ab6764c8cd453
Yagorus <yegor.sidenko@gmail.com>
2021-12-29 08:33:34 +0200

    asdf

--- lb.tf
+++ lb.tf
@@ -13,7 +13,7 @@ resource "aws_alb_target_group" "app" {
   port        = 80
   protocol    = "HTTP"
   vpc_id      = aws_vpc.main.id
-  target_type = "ip"
+  #target_type = "ip"
 
   health_check {
     healthy_threshold   = "3"
